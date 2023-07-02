991c369a66fbb991c43915a2b96effa4c7aaf351
Arthur Benilov <arthur.benilov@gmail.com>
2023-07-02 23:08:00 +0100

    Simplify loader.

--- vitro/core/vitro_Loader.cpp
+++ vitro/core/vitro_Loader.cpp
@@ -1,34 +1,9 @@
 namespace vitro {
 
-// This represents a location split into scheme and path.
-struct SchemeAndPath
-{
-    String scheme;
-    String path;
-};
-
-static SchemeAndPath splitLocation(const String& location)
-{
-    SchemeAndPath snp{};
-
-    const auto separator{ location.indexOf("://") };
-
-    if (separator < 0) {
-        snp.path = location;
-    } else {
-        snp.scheme = location.substring (0, separator).trim();
-        snp.path = location.substring (separator + 3).trim();
-    }
-
-    return snp;
-}
-
-
-static const char* getBinaryDataResourceNameForFile (const String& filename)
+static const char* getBinaryDataResourceNameForFile(const String& filename)
 {
 #if JUCE_TARGET_HAS_BINARY_DATA
-    for (int i = 0; i < BinaryData::namedResourceListSize; ++i)
-    {
+    for (int i = 0; i < BinaryData::namedResourceListSize; ++i) {
         if (filename == BinaryData::originalFilenames[i])
             return BinaryData::namedResourceList[i];
     }
@@ -51,18 +26,15 @@ void Loader::setLocalDirectory(const juce::File& dir)
 
 String Loader::loadText(const String& location) const
 {
-    const auto snp {splitLocation (location)};
-
     if (hasLocalPath) {
-        auto file{ localPath.getChildFile(snp.path) };
+        auto file{ localPath.getChildFile(location) };
 
         if (file.existsAsFile())
             return file.loadFileAsString();
     }
 
 #if JUCE_TARGET_HAS_BINARY_DATA
-    if (auto* resourceName{ getBinaryDataResourceNameForFile(location) })
-    {
+    if (auto* resourceName{ getBinaryDataResourceNameForFile(location) }) {
         int size{};
 
         if (const auto* data{ BinaryData::getNamedResource(resourceName, size) })
@@ -75,25 +47,23 @@ String Loader::loadText(const String& location) const
 
 std::unique_ptr<XmlElement> Loader::loadXML(const juce::String& location) const
 {
-    const auto str{ loadText (location) };
-    return XmlDocument::parse (str);
+    const auto str{ loadText(location) };
+    return XmlDocument::parse(str);
 }
 
 juce::Image Loader::loadImage(const String& location) const
 {
     const auto hash{ location.hashCode64() };
-    auto image{ ImageCache::getFromHashCode (hash) };
+    auto image{ ImageCache::getFromHashCode(hash) };
 
     if (image.isValid())
         return image;
 
-    const auto snp{ splitLocation (location) };
-
     if (hasLocalPath) {
-        auto file{ localPath.getChildFile (snp.path) };
+        auto file{ localPath.getChildFile(location) };
 
         if (file.existsAsFile())
-            image = ImageFileFormat::loadFrom (file);
+            image = ImageFileFormat::loadFrom(file);
     }
 
 #if JUCE_TARGET_HAS_BINARY_DATA
@@ -103,7 +73,7 @@ juce::Image Loader::loadImage(const String& location) const
             int size{};
 
             if (const auto* data{ BinaryData::getNamedResource(resourceName, size) })
-                image = ImageFileFormat::loadFrom(data, size_t (size));
+                image = ImageFileFormat::loadFrom(data, size_t(size));
         }
     }
 #endif
