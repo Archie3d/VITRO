61569efaea5e6b28cfa10bbb042a6cc08e407cdf
Arthur Benilov <arthur.benilov@gmail.com>
2023-11-09 13:30:09 +0000

    Show scroll bar over the content in ScrollArea.

--- vitro/widgets/vitro_ScrollArea.cpp
+++ vitro/widgets/vitro_ScrollArea.cpp
@@ -4,6 +4,24 @@ JSClassID ScrollArea::jsClassID = 0;
 
 const Identifier ScrollArea::tag("ScrollArea");
 
+namespace {
+    constexpr int kScrollBarShowTimeMs = 1500;
+    constexpr int kScrollBarFadeOutTimeMs = 500;
+    constexpr int kScrollBarSingleStepSize = 16;
+}
+
+//==============================================================================
+
+ScrollArea::Updater::Updater(ScrollArea& parent)
+    : scrollArea{ parent }
+{
+}
+
+void ScrollArea::Updater::scrollBarMoved(juce::ScrollBar* bar, double newRangeStart)
+{
+    scrollArea.customScrollBarMoved(bar, newRangeStart);
+}
+
 //==============================================================================
 
 void ScrollArea::Container::childBoundsChanged(juce::Component* child)
@@ -14,9 +32,21 @@ void ScrollArea::Container::childBoundsChanged(juce::Component* child)
 //==============================================================================
 
 ScrollArea::ScrollArea(Context& ctx)
-    : ComponentElement(ScrollArea::tag, ctx)
+    : ComponentElement(ScrollArea::tag, ctx),
+      scrollUpdater{ *this }
 {
+    // Hide the viewport scroll bars but allow scrolling
+    setScrollBarsShown(false, false, true, true);
+
+    addChildComponent(horizontalScrollBar);
+    addChildComponent(verticalScrollBar);
+
+    horizontalScrollBar.addListener(&scrollUpdater);
+    verticalScrollBar.addListener(&scrollUpdater);
+
     setViewedComponent(&container, false);
+
+    addMouseListener(this, true);
 
     registerStyleProperty(attr::css::thumb_color);
     registerStyleProperty(attr::css::vertical_scrollbar);
@@ -46,11 +76,104 @@ void ScrollArea::update()
     }
 
     if (showScrollbarChanged)
-        setScrollBarsShown(showVeticalScrollbar, showHorizontalScrollbar);
+    {
+        horizontalScrollBar.setVisible(showHorizontalScrollbar);
+        verticalScrollBar.setVisible(showVeticalScrollbar);
+    }
 
     // scrollbar-thickness
     if (auto&& [changed, prop]{ getStylePropertyChanged(attr::css::scrollbar_thickness) }; changed)
+    {
+        // We store thickness in the native scrollbars, but they are not visible.
+        // The thickness will be retrieved then via getScrollBarThickness().
         setScrollBarThickness(prop.isVoid() ? 8 : (int)prop);
+    }
+
+    updateScrollBars();
+}
+
+void ScrollArea::resized()
+{
+    juce::Viewport::resized();
+    updateScrollBars();
+}
+
+void ScrollArea::visibleAreaChanged(const juce::Rectangle<int>& newVisibleArea)
+{
+    updateScrollBars();
+    triggerShowScrollBars();
+}
+
+void ScrollArea::mouseMove(const juce::MouseEvent& e)
+{
+    mousePosition = e.getEventRelativeTo(this).getPosition();
+    updateScrollBarsVisibility();
+}
+
+void ScrollArea::timerCallback()
+{
+    stopTimer();
+    updateScrollBarsVisibility();
+}
+
+void ScrollArea::updateScrollBars()
+{
+    const auto viewportBounds{ getBounds() };
+    const auto scrollBarWidth{ getScrollBarThickness() };
+    const auto contentBounds{ getViewedComponent()->getBounds() };
+
+    bool rangeLimitChanged = horizontalScrollBar.getRangeLimit().getEnd() != contentBounds.getWidth();
+    
+    horizontalScrollBar.setBounds(0, viewportBounds.getHeight() - scrollBarWidth, viewportBounds.getWidth(), scrollBarWidth);
+    horizontalScrollBar.setRangeLimits(0.0, contentBounds.getWidth());
+    horizontalScrollBar.setCurrentRange(getViewPositionX(), viewportBounds.getWidth());
+    horizontalScrollBar.setSingleStepSize(kScrollBarSingleStepSize);
+    horizontalScrollBar.cancelPendingUpdate();
+
+    rangeLimitChanged = rangeLimitChanged || verticalScrollBar.getRangeLimit().getEnd() != contentBounds.getHeight();
+    
+    verticalScrollBar.setBounds(viewportBounds.getWidth() - scrollBarWidth, 0, scrollBarWidth, viewportBounds.getHeight());
+    verticalScrollBar.setRangeLimits(0.0, contentBounds.getHeight());
+    verticalScrollBar.setCurrentRange(getViewPositionY(), viewportBounds.getHeight());
+    verticalScrollBar.setSingleStepSize(kScrollBarSingleStepSize);
+    verticalScrollBar.cancelPendingUpdate();
+    
+    if (rangeLimitChanged)
+        triggerShowScrollBars();
+}
+
+void ScrollArea::triggerShowScrollBars()
+{
+    startTimer(kScrollBarShowTimeMs);
+    updateScrollBarsVisibility();
+}
+
+void ScrollArea::customScrollBarMoved(juce::ScrollBar* bar, double newRangeStart)
+{
+    int newRangeStartInt{ juce::roundToInt(newRangeStart) };
+
+    if (bar == &horizontalScrollBar) {
+        setViewPosition(newRangeStartInt, getViewPositionY());
+    } else if (bar == &verticalScrollBar) {
+        setViewPosition(getViewPositionX(), newRangeStartInt);
+    }
+}
+
+void ScrollArea::updateScrollBarsVisibility()
+{
+    for (auto* bar : { &horizontalScrollBar, &verticalScrollBar }) {
+        const bool currentlyVisible{ bar->isVisible() };
+        //const bool shouldBeVisible{ isTimerRunning() || bar->getBounds().contains(mousePosition) };
+        
+        const bool shouldBeVisible{ isTimerRunning() };
+        
+        if (currentlyVisible && !shouldBeVisible) {
+            animator.fadeOut(bar, kScrollBarFadeOutTimeMs);
+        } else {
+            bar->setVisible(shouldBeVisible);
+            bar->setAlpha(shouldBeVisible ? 1.0f : 0.0f);
+        }
+    }
 }
 
 } // namespace Label
--- vitro/widgets/vitro_ScrollArea.h
+++ vitro/widgets/vitro_ScrollArea.h
@@ -15,7 +15,8 @@ namespace vitro {
         scrollbar-thickness
 */
 class ScrollArea : public vitro::ComponentElement,
-                   public juce::Viewport
+                   public juce::Viewport,
+                   private juce::Timer
 {
 public:
 
@@ -31,12 +32,30 @@ public:
 
     juce::Component* getContainerComponent() override { return &container; }
 
+    // juce::Viewport
+    void resized() override;
+    void visibleAreaChanged(const juce::Rectangle<int>& newVisibleArea) override;
+    void mouseMove(const juce::MouseEvent& e) override;
+
 protected:
 
     // vitro::Element
     void update() override;
 
 private:
+
+    class Updater : public juce::ScrollBar::Listener
+    {
+    public:
+        Updater(ScrollArea& parent);
+
+        void scrollBarMoved(juce::ScrollBar* bar, double newRangeStart) override;
+    private:
+
+        ScrollArea& scrollArea;
+    };
+
+    friend class Updater;
 
     // Component with the s scrollable content.
     // This component automatically adjusts its size to its child element.
@@ -47,9 +66,31 @@ private:
         void childBoundsChanged(juce::Component* child) override;
     };
 
+    void updateScrollBars();
+
+    void triggerShowScrollBars();
+
+    // juce::Timer
+    void timerCallback() override;
+
+    // @internal
+    // To be called by the Updater
+    void customScrollBarMoved(juce::ScrollBar* bar, double newRangeStart);
+
+    void updateScrollBarsVisibility();
+
+    Updater scrollUpdater;
+
     // Scrollable component
     Container container;
 
+    juce::ScrollBar horizontalScrollBar{ false };
+    juce::ScrollBar verticalScrollBar{ true };
+
+    // Animator to fade out the scroll bars
+    juce::ComponentAnimator animator{};
+
+    juce::Point<int> mousePosition{};
 };
 
 } // namespace vitro
